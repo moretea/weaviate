0db466503820a335cb7101ba2ee2625c61e31fe8
Maarten Hoogendoorn <maarten@moretea.nl>
2018-07-17 13:21:34 +0200

    gh-390: Poll until Cassandra is up; don't wait.

--- test/run_acceptance_tests.sh
+++ test/run_acceptance_tests.sh
@@ -2,8 +2,15 @@ set -e
 set -m
 
 # Running Cassandra deamon
-docker run -it --name weaviate_db_1_travis -e CASSANDRA_BROADCAST_ADDRESS=127.0.0.1 -p 7000:7000 -p 9042:9042 -d cassandra:3
-sleep 45
+if (docker ps --format '{{ .Names }}' | grep  -s weaviate_db_1_travis); then
+  echo "Deleting weaviate_db_1_travis"
+  docker rm -f weaviate_db_1_travis > /dev/null
+fi
+
+echo "Starting Cassandra..."
+docker run --rm -it --name weaviate_db_1_travis -e CASSANDRA_BROADCAST_ADDRESS=127.0.0.1 -p 7000:7000 -p 9042:9042 -d cassandra:3 > /dev/null
+docker exec weaviate_db_1_travis bash -c "echo 'waiting until Cassandra is up...'; while ! (cqlsh -e 'describe cluster' 1>&2 2> /dev/null) ; do sleep 1; done; echo 'it is up!'"
+
 # Running Weaviate as deamon
 nohup go run ./cmd/weaviate-server/main.go --scheme=http --port=8080 --host=127.0.0.1 --config="cassandra_docker" --config-file="./weaviate.conf.json" &
 # Sleep to make sure all is up and running
