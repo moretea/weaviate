f90ba21b1976153795c66b53e1bf903273bec812
Maarten Hoogendoorn <maarten@moretea.nl>
2018-09-06 12:23:55 +0200

    Improve assertion failure message in acceptance tests

--- test/acceptance/helper/assertions.go
+++ test/acceptance/helper/assertions.go
@@ -9,8 +9,9 @@ import (
 // Optionally perform some checks only if the request did not fail
 func AssertRequestOk(t *testing.T, response interface{}, err error, check_fn func()) {
 	if err != nil {
+		error_json, _ := json.MarshalIndent(err, "", "  ")
 		response_json, _ := json.MarshalIndent(response, "", "  ")
-		t.Fatalf("Failed to perform request, because %#v. Response:\n%s", err, response_json)
+		t.Fatalf("Failed to perform request, because %#v. Response:\n%s", string(error_json), string(response_json))
 	} else {
 		if check_fn != nil {
 			check_fn()
